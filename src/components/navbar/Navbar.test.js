793a24ee55957d627046faf1a4c57bc37c1fa0e3
redapy <baha.redaaaa@gmail.com>
2022-03-19 15:47:15 +0100

    wrap the navbar component with context provider inside the test file

--- src/components/navbar/Navbar.test.js
+++ src/components/navbar/Navbar.test.js
@@ -1,33 +1,36 @@
 import Navbar from "./Navbar";
 import { MemoryRouter } from "react-router-dom";
 import { render, screen } from "@testing-library/react";
+import { AuthContextProvider } from "../../context/AuthContext";
 
 describe("Navbar component", () => {
-  const NavbarWithRouter = () => {
+  const MockedNavbar = () => {
     return (
-      <MemoryRouter>
-        <Navbar />
-      </MemoryRouter>
+      <AuthContextProvider>
+        <MemoryRouter>
+          <Navbar />
+        </MemoryRouter>
+      </AuthContextProvider>
     );
   };
   describe("Renders", () => {
     it("Render a navbar component", () => {
-      render(<NavbarWithRouter />);
+      render(<MockedNavbar />);
       const navbar = screen.getByTestId("navbar");
       expect(navbar).toBeInTheDocument();
     });
     it("Render a logo title", () => {
-      render(<NavbarWithRouter />);
+      render(<MockedNavbar />);
       const logo = screen.getByText(/myMoney/i);
       expect(logo).toBeInTheDocument();
     });
     it("Render a link to the signup page", () => {
-      render(<NavbarWithRouter />);
+      render(<MockedNavbar />);
       const signupLink = screen.getByText(/signup/i);
       expect(signupLink).toBeInTheDocument();
     });
     it("Render a link to the login page", () => {
-      render(<NavbarWithRouter />);
+      render(<MockedNavbar />);
       const loginLink = screen.getByText(/login/i);
       expect(loginLink).toBeInTheDocument();
     });
