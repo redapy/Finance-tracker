e721d2cd38616d7f83c912dfbeb599e8071f68e1
redapy <baha.redaaaa@gmail.com>
2022-03-21 19:58:05 +0100

    output the navbar elements conditionally * depending on the user state

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,27 +1,38 @@
 import { Link } from "react-router-dom";
+import { useAuthContext } from "../../hooks/useAuthContext";
 import { useSignOut } from "../../hooks/useSignOut";
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   const { logout } = useSignOut();
+  const { user } = useAuthContext();
   return (
     <nav data-testid="navbar" className={styles.navbar}>
       <ul>
         <li className={styles.title}>
           <Link to="/">myMoney</Link>
         </li>
-
-        <li>
-          <Link to="/login">Login</Link>
-        </li>
-        <li>
-          <Link to="/signup">Signup</Link>
-        </li>
-        <li>
-          <button className="btn" onClick={logout}>
-            Log out
-          </button>
-        </li>
+        {user ? (
+          <>
+            <p>
+              Hello, <span>{user.displayName}</span>
+            </p>
+            <li>
+              <button className="btn" onClick={logout}>
+                Log out
+              </button>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
