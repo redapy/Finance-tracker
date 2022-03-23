c1c5849d5cf4215f1ae0d5e883a5d54b91e1cde2
redapy <baha.redaaaa@gmail.com>
2022-03-23 17:06:16 +0100

    create the transaction form and the home page template

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,14 @@
 import styles from "./Home.module.css";
+import TransactionForm from "./TransactionForm";
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>transaction list</div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
